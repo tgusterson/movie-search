a8f038f9df995f4c2cc47b203582545b64281dea
tgusterson <47345128+tgusterson@users.noreply.github.com>
2020-02-05 17:44:45 +1300

    Update search.js

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,6 +1,7 @@
 const request = require('request')
 
 module.exports = (movie, page = 1, callback) => {
+  // YOUR OWN TMDB API KEY SHOULD REPLACE API_KEY
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(movie)}&page=${page}`;
 
   request({ url, json: true }, (error, { body } = {}) => {
